aeb94933622a89e568e1d7c989d5df2568d56749
alonj <alon38@gmail.com>
2018-06-21 21:18:03 +0300

    logic fully implemented, edge case testing remains

--- Airplane.h
+++ Airplane.h
@@ -16,7 +16,7 @@ template<typename T> class Airplane
 		Airplane( const Airplane& otherAirplane ) :_name(otherAirplane.getName()),
 												   _load(otherAirplane.getLoad()){};
 
-		~Airplane();
+		~Airplane(){};
 
 		bool	isEqual( const string& name ) const;
 		bool	isEqual( const Airplane& otherAirplane ) const;
@@ -31,6 +31,26 @@ template<typename T> class Airplane
 //*********************************************************************
 //********************** YOUR CODE STARTS HERE ************************
 //*********************************************************************
+
+template<typename T>
+string Airplane<T>::getName() const {
+    return _name;
+}
+
+template<typename T>
+T Airplane<T>::getLoad() const {
+    return _load;
+}
+
+template<typename T>
+bool Airplane<T>::isEqual(const string &name) const {
+    return _name == name;
+}
+
+template<typename T>
+bool Airplane<T>::isEqual(const Airplane &otherAirplane) const {
+    return _name == otherAirplane.getName();
+}
 
 
 #endif
--- Terminal.h
+++ Terminal.h
@@ -28,11 +28,13 @@ private:
 
 public:
 	Terminal(const string& termnType, T maxTermnLoad ): _termnType(termnType),
-														_maxTermnLoad(maxTermnLoad){};
+														_maxTermnLoad(maxTermnLoad)
+	{_terminalLoad = 0;};
 	Terminal(const Terminal& otherTerminal): _termnType(otherTerminal.get_termnType()),
-											_maxTermnLoad(otherTerminal.get_maxTermnLoad()){};
+											_maxTermnLoad(otherTerminal.get_maxTermnLoad())
+	{_terminalLoad = 0;};
 
-	~Terminal();
+	~Terminal(){};
 
 	void beginSession();
 
@@ -55,6 +57,85 @@ public:
 //*********************************************************************
 //********************** YOUR CODE STARTS HERE ************************
 //*********************************************************************
+
+template<typename T>
+bool Terminal<T>::addPlane(Airplane<T> *newAirplane) {
+	if(this->hasPlane(newAirplane->getName())) {
+		cout << "The plane is already registered: " << newAirplane->getName() << endl;
+		return false;
+	}
+	else if (_terminalLoad + newAirplane->getLoad() > _maxTermnLoad) {
+			cout << "Not enough free space for: " << newAirplane->getName() << endl;
+			return false;
+		}
+	else{
+		_vecAirplanes.push_back(newAirplane);
+		_terminalLoad += newAirplane->getLoad();
+		return true;
+	}
+}
+
+template<typename T>
+bool Terminal<T>::delPlane(Airplane<T> *newAirplane) {
+	string name = newAirplane->getName();
+	bool planeRegistered = false;
+	typename vector<Airplane<T>*>::iterator it = _vecAirplanes.begin();
+	for(; it != _vecAirplanes.end(); it++)
+		if((*it)->isEqual(name)){
+			planeRegistered = true;
+			break;
+		}
+	if(!planeRegistered){
+		cout << "The plane was not registered: " << newAirplane->getName() << endl;
+		return false;
+	}
+	else if (_terminalLoad < newAirplane->getLoad()){
+		cout << "Not enough allocated objects for: " << newAirplane->getName() << endl;
+		return false;
+	}
+	else{
+		_vecAirplanes.erase(it);
+		_terminalLoad -= newAirplane->getLoad();
+		return true;
+	}
+}
+
+template<typename T>
+bool Terminal<T>::hasPlane(const string &name) const {
+	for(typename vector< Airplane<T>* >::const_iterator cit = _vecAirplanes.begin(); cit != _vecAirplanes.end(); cit++)
+		if((*cit)->isEqual(name))
+			return true;
+	return false;
+}
+
+template<typename T>
+void Terminal<T>::print() const {
+	if(_vecAirplanes.empty()) return;
+	typename vector< Airplane<T>*>::const_iterator cit = _vecAirplanes.begin();
+	while(cit != _vecAirplanes.end())
+		cout << (*cit++)->getName() << endl;
+}
+
+template<typename T>
+void Terminal<T>::beginSession() {
+	string line, mode, name;
+	T load;
+	bool funcReturn = true;
+	while(!cin.eof()){
+		getline(cin, line);
+		std::stringstream ss(line);
+		ss >> mode >> name >> load;
+		Airplane<T>* newAirplane = new Airplane<T>(name, load);
+		if(mode == A)
+			funcReturn = addPlane(newAirplane);
+		else if(mode == D)
+			funcReturn = delPlane(newAirplane);
+		else if(mode == PRINT)
+			print();
+		if(!funcReturn)
+			cout << "Unknown error occurred while handling: " << name << endl;
+	}
+}
 
 
 #endif
