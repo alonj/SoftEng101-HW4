d41ac24cc839533520cc49c54725795df3c948cc
alonj <alon38@gmail.com>
2018-06-21 22:24:26 +0300

    logic fully implemented

--- Airplane.h
+++ Airplane.h
@@ -1,27 +1,32 @@
 #ifndef AIRPLANE_H
 #define AIRPLANE_H
- 
+
 #include <string>
 
 using std::string;
 
-template<typename T> class Airplane
+template<typename T>
+class Airplane
 {
-	private:
-		const string _name;
-		T			 _load;
+private:
+    const string _name;
+    T _load;
 
-	public:
-		Airplane( const string& name, const T& load ) : _name(name), _load(load) {};
-		Airplane( const Airplane& otherAirplane ) :_name(otherAirplane.getName()),
-												   _load(otherAirplane.getLoad()){};
+public:
+    Airplane(const string &name, const T &load) : _name(name), _load(load){};
 
-		~Airplane(){};
+    Airplane(const Airplane &otherAirplane) : _name(otherAirplane.getName()),
+                                              _load(otherAirplane.getLoad()){};
 
-		bool	isEqual( const string& name ) const;
-		bool	isEqual( const Airplane& otherAirplane ) const;
-		string	getName() const;
-		T		getLoad() const;
+    ~Airplane(){};
+
+    bool isEqual(const string &name) const;
+
+    bool isEqual(const Airplane &otherAirplane) const;
+
+    string getName() const;
+
+    T getLoad() const;
 
 };
 
@@ -33,22 +38,26 @@ template<typename T> class Airplane
 //*********************************************************************
 
 template<typename T>
-string Airplane<T>::getName() const {
+string Airplane<T>::getName() const
+{
     return _name;
 }
 
 template<typename T>
-T Airplane<T>::getLoad() const {
+T Airplane<T>::getLoad() const
+{
     return _load;
 }
 
 template<typename T>
-bool Airplane<T>::isEqual(const string &name) const {
+bool Airplane<T>::isEqual(const string &name) const
+{
     return _name == name;
 }
 
 template<typename T>
-bool Airplane<T>::isEqual(const Airplane &otherAirplane) const {
+bool Airplane<T>::isEqual(const Airplane &otherAirplane) const
+{
     return _name == otherAirplane.getName();
 }
 
--- Terminal.h
+++ Terminal.h
@@ -1,6 +1,6 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
- 
+
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -21,36 +21,41 @@ template<typename T>
 class Terminal
 {
 private:
-	string					_termnType;
-	const T					_maxTermnLoad;
-	vector< Airplane<T>* >	_vecAirplanes;
-	T						_terminalLoad;
+    string _termnType;
+    const T _maxTermnLoad;
+    vector<Airplane<T> *> _vecAirplanes;
+    T _terminalLoad;
 
 public:
-	Terminal(const string& termnType, T maxTermnLoad ): _termnType(termnType),
-														_maxTermnLoad(maxTermnLoad)
-	{_terminalLoad = 0;};
-	Terminal(const Terminal& otherTerminal): _termnType(otherTerminal.get_termnType()),
-											_maxTermnLoad(otherTerminal.get_maxTermnLoad())
-	{_terminalLoad = 0;};
+    Terminal(const string &termnType, T maxTermnLoad) : _termnType(termnType),
+                                                        _maxTermnLoad(maxTermnLoad)
+    { _terminalLoad = 0; };
 
-	~Terminal(){};
+    Terminal(const Terminal &otherTerminal) : _termnType(otherTerminal.get_termnType()),
+                                              _maxTermnLoad(otherTerminal.get_maxTermnLoad())
+    { _terminalLoad = 0; };
 
-	void beginSession();
+    ~Terminal(){};
 
-	bool addPlane( Airplane<T>* newAirplane );
-	bool delPlane( Airplane<T>* newAirplane );
-	bool hasPlane( const string& name ) const;
+    void beginSession();
 
-	const string &get_termnType() const {
-		return _termnType;
-	}
+    bool addPlane(Airplane<T> *newAirplane);
 
-	const T get_maxTermnLoad() const {
-		return _maxTermnLoad;
-	}
+    bool delPlane(Airplane<T> *newAirplane);
 
-	void print() const;
+    bool hasPlane(const string &name) const;
+
+    const string &get_termnType() const
+    {
+        return _termnType;
+    }
+
+    const T get_maxTermnLoad() const
+    {
+        return _maxTermnLoad;
+    }
+
+    void print() const;
 };
 
 
@@ -59,82 +64,96 @@ public:
 //*********************************************************************
 
 template<typename T>
-bool Terminal<T>::addPlane(Airplane<T> *newAirplane) {
-	if(this->hasPlane(newAirplane->getName())) {
-		cout << "The plane is already registered: " << newAirplane->getName() << endl;
-		return false;
-	}
-	else if (_terminalLoad + newAirplane->getLoad() > _maxTermnLoad) {
-			cout << "Not enough free space for: " << newAirplane->getName() << endl;
-			return false;
-		}
-	else{
-		_vecAirplanes.push_back(newAirplane);
-		_terminalLoad += newAirplane->getLoad();
-		return true;
-	}
+bool Terminal<T>::addPlane(Airplane<T> *newAirplane)
+{
+    if (this->hasPlane(newAirplane->getName()))
+    {
+        cout << "The plane is already registered: " << newAirplane->getName() << endl;
+        return false;
+    }
+    else if (_terminalLoad + newAirplane->getLoad() > _maxTermnLoad)
+    {
+        cout << "Not enough free space for: " << newAirplane->getName() << endl;
+        return false;
+    }
+    else
+    {
+        _vecAirplanes.push_back(newAirplane);
+        _terminalLoad += newAirplane->getLoad();
+        return true;
+    }
 }
 
 template<typename T>
-bool Terminal<T>::delPlane(Airplane<T> *newAirplane) {
-	string name = newAirplane->getName();
-	bool planeRegistered = false;
-	typename vector<Airplane<T>*>::iterator it = _vecAirplanes.begin();
-	for(; it != _vecAirplanes.end(); it++)
-		if((*it)->isEqual(name)){
-			planeRegistered = true;
-			break;
-		}
-	if(!planeRegistered){
-		cout << "The plane was not registered: " << newAirplane->getName() << endl;
-		return false;
-	}
-	else if (_terminalLoad < newAirplane->getLoad()){
-		cout << "Not enough allocated objects for: " << newAirplane->getName() << endl;
-		return false;
-	}
-	else{
-		_vecAirplanes.erase(it);
-		_terminalLoad -= newAirplane->getLoad();
-		return true;
-	}
+bool Terminal<T>::delPlane(Airplane<T> *newAirplane)
+{
+    string name = newAirplane->getName();
+    bool planeRegistered = false;
+    typename vector<Airplane<T> *>::iterator it = _vecAirplanes.begin();
+    for (; it != _vecAirplanes.end(); it++)
+        if ((*it)->isEqual(name))
+        {
+            planeRegistered = true;
+            break;
+        }
+    if (!planeRegistered)
+    {
+        cout << "The plane was not registered: " << newAirplane->getName() << endl;
+        return false;
+    }
+    else if (_terminalLoad < newAirplane->getLoad())
+    {
+        cout << "Not enough allocated objects for: " << newAirplane->getName() << endl;
+        return false;
+    }
+    else
+    {
+        _vecAirplanes.erase(it);
+        _terminalLoad -= newAirplane->getLoad();
+        return true;
+    }
 }
 
 template<typename T>
-bool Terminal<T>::hasPlane(const string &name) const {
-	for(typename vector< Airplane<T>* >::const_iterator cit = _vecAirplanes.begin(); cit != _vecAirplanes.end(); cit++)
-		if((*cit)->isEqual(name))
-			return true;
-	return false;
+bool Terminal<T>::hasPlane(const string &name) const
+{
+    for (typename vector<Airplane<T> *>::const_iterator cit = _vecAirplanes.begin(); cit != _vecAirplanes.end(); cit++)
+        if ((*cit)->isEqual(name))
+            return true;
+    return false;
 }
 
 template<typename T>
-void Terminal<T>::print() const {
-	if(_vecAirplanes.empty()) return;
-	typename vector< Airplane<T>*>::const_iterator cit = _vecAirplanes.begin();
-	while(cit != _vecAirplanes.end())
-		cout << (*cit++)->getName() << endl;
+void Terminal<T>::print() const
+{
+    if (_vecAirplanes.empty()) return;
+    typename vector<Airplane<T> *>::const_iterator cit = _vecAirplanes.begin();
+    while (cit != _vecAirplanes.end())
+        cout << (*cit++)->getName() << endl;
 }
 
 template<typename T>
-void Terminal<T>::beginSession() {
-	string line, mode, name;
-	T load;
-	bool funcReturn = true;
-	while(!cin.eof()){
-		getline(cin, line);
-		std::stringstream ss(line);
-		ss >> mode >> name >> load;
-		Airplane<T>* newAirplane = new Airplane<T>(name, load);
-		if(mode == A)
-			funcReturn = addPlane(newAirplane);
-		else if(mode == D)
-			funcReturn = delPlane(newAirplane);
-		else if(mode == PRINT)
-			print();
-		if(!funcReturn)
-			cout << "Unknown error occurred while handling: " << name << endl;
-	}
+void Terminal<T>::beginSession()
+{
+    string line, mode, name;
+    T load;
+    bool funcReturn = false;
+    while (!cin.eof())
+    {
+        getline(cin, line);
+        std::stringstream ss(line);
+        ss >> mode >> name >> load;
+        Airplane<T> *newAirplane = new Airplane<T>(name, load);
+        if (mode == A)
+            funcReturn = addPlane(newAirplane);
+        else if (mode == D)
+            funcReturn = delPlane(newAirplane);
+        else if (mode == PRINT)
+            print();
+        if (!funcReturn)
+            delete newAirplane;
+//		cout << "Current terminal status: " << this->_termnType << "[" << this->_terminalLoad << " / " << this->_maxTermnLoad << "]" << endl;
+    }
 }
 
 
